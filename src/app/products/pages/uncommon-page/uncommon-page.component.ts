f6eb2a58623abd388981dbfaee21f5c0479825b9
CarlosReyesMelero <creyesmelero@gmail.com>
2024-08-06 20:04:22 +0200

    i18nSelect and i18Plural Pipes

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+  // i18n Select
+  public name: string = 'Carlos';
+  public gender: 'male'|'female' = 'male';
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla'
+  }
+
+  public changeClient():void {
+    this.name = 'Melissa'
+    this.gender = 'female'
+  }
+
+  // i18n Plural
+  public clients: string[] = ['Maria', 'Carlos', 'Reyes', 'Araceli', 'Juanma', 'Sergio', 'Pepe'];
+  public clientsMap = {
+    '=0': 'no quedan clientes esperando',
+    '=1': 'tenemos un cliente esperando',
+    'other': 'tenemos # clientes esperando',
+  }
+
+  public deleteClient(): void {
+    this.clients.shift();
+  }
 }
